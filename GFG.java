865ebec88833c9ada28be14434a35420a75c6de1
rishika251 <rishikasadineni@gmail.com>
2022-09-26 10:30:58 +0530

    [rishika] -program to check GFG.java

---
+++ GFG.java
@@ -0,0 +1,10 @@
+public class GFG {
+	static int findSum(int n) {
+		return n * (n+1)/2;
+		}
+	public static void main(String[] args) {
+		int n = 5;
+		System.out.println("findSum(n)");
+   }
+
+}
